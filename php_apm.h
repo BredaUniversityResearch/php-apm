b811e36be2b22f4d0cb64d7277a4d48e67454ab3
Marin <hekman.m@buas.nl>
2025-11-13 23:58:16 +0100

    fixed more errors

--- php_apm.h
+++ php_apm.h
@@ -76,15 +76,15 @@ typedef struct apm_event_entry {
 
 typedef struct apm_driver {
 	void (* process_event)(PROCESS_EVENT_ARGS);
-	void (* process_stats);
-	int (* minit)(int );
-	int (* rinit);
+	void (* process_stats)(void);
+	int (* minit)(int);
+	int (* rinit)(void);
 	int (* mshutdown)(SHUTDOWN_FUNC_ARGS);
-	int (* rshutdown);
-	zend_bool (* is_enabled);
+	int (* rshutdown)(void);
+	zend_bool (* is_enabled)(void);
 	zend_bool (* want_event)(int, int, char * );
-	zend_bool (* want_stats);
-	int (* error_reporting);
+	zend_bool (* want_stats)(void);
+	int (* error_reporting)(void);
 	zend_bool is_request_created;
 } apm_driver;
 
@@ -127,64 +127,64 @@ typedef struct apm_request_data {
 
 #define APM_DRIVER_CREATE(name) \
 void apm_driver_##name##_process_event(PROCESS_EVENT_ARGS); \
-void apm_driver_##name##_process_stats; \
-int apm_driver_##name##_minit(int ); \
-int apm_driver_##name##_rinit; \
-int apm_driver_##name##_mshutdown(); \
-int apm_driver_##name##_rshutdown; \
+void apm_driver_##name##_process_stats(void); \
+int apm_driver_##name##_minit(int); \
+int apm_driver_##name##_rinit(void); \
+int apm_driver_##name##_mshutdown(SHUTDOWN_FUNC_ARGS); \
+int apm_driver_##name##_rshutdown(void); \
 PHP_INI_MH(OnUpdateAPM##name##ErrorReporting) \
 { \
-	APM_GLOBAL(name, error_reporting) = (apm_error_reporting_new_value : APM_E_##name); \
-	return SUCCESS; \
+    APM_GLOBAL(name, error_reporting) = (apm_error_reporting_new_value : APM_E_##name); \
+    return SUCCESS; \
 } \
-zend_bool apm_driver_##name##_is_enabled \
+zend_bool apm_driver_##name##_is_enabled(void) \
 { \
-	return APM_GLOBAL(name, enabled); \
+    return APM_GLOBAL(name, enabled); \
 } \
-int apm_driver_##name##_error_reporting \
+int apm_driver_##name##_error_reporting(void) \
 { \
-	return APM_GLOBAL(name, error_reporting); \
+    return APM_GLOBAL(name, error_reporting); \
 } \
 zend_bool apm_driver_##name##_want_event(int event_type, int error_level, char *msg) \
 { \
-	return \
-		APM_GLOBAL(name, enabled) \
-		&& ( \
-			(event_type == APM_EVENT_EXCEPTION && APM_GLOBAL(name, exception_mode) == 2) \
-			|| \
-			(event_type == APM_EVENT_ERROR && ((APM_GLOBAL(name, exception_mode) == 1) || (strncmp(msg, "Uncaught exception", 18) != 0)) && (error_level & APM_GLOBAL(name, error_reporting))) \
-		) \
-		&& ( \
-			!APM_G(currently_silenced) || APM_GLOBAL(name, process_silenced_events) \
-		) \
-	; \
+    return \
+        APM_GLOBAL(name, enabled) \
+        && ( \
+            (event_type == APM_EVENT_EXCEPTION && APM_GLOBAL(name, exception_mode) == 2) \
+            || \
+            (event_type == APM_EVENT_ERROR && ((APM_GLOBAL(name, exception_mode) == 1) || (strncmp(msg, "Uncaught exception", 18) != 0)) && (error_level & APM_GLOBAL(name, error_reporting))) \
+        ) \
+        && ( \
+            !APM_G(currently_silenced) || APM_GLOBAL(name, process_silenced_events) \
+        ) \
+    ; \
 } \
-zend_bool apm_driver_##name##_want_stats \
+zend_bool apm_driver_##name##_want_stats(void) \
 { \
-	return \
-		APM_GLOBAL(name, enabled) \
-		&& ( \
-			APM_GLOBAL(name, stats_enabled)\
-		) \
-	; \
+    return \
+        APM_GLOBAL(name, enabled) \
+        && ( \
+            APM_GLOBAL(name, stats_enabled)\
+        ) \
+    ; \
 } \
-apm_driver_entry * apm_driver_##name##_create() \
+apm_driver_entry * apm_driver_##name##_create(void) \
 { \
-	apm_driver_entry * driver_entry; \
-	driver_entry = (apm_driver_entry *) malloc(sizeof(apm_driver_entry)); \
-	driver_entry->driver.process_event = apm_driver_##name##_process_event; \
-	driver_entry->driver.minit = apm_driver_##name##_minit; \
-	driver_entry->driver.rinit = apm_driver_##name##_rinit; \
-	driver_entry->driver.mshutdown = apm_driver_##name##_mshutdown; \
-	driver_entry->driver.rshutdown = apm_driver_##name##_rshutdown; \
-	driver_entry->driver.process_stats = apm_driver_##name##_process_stats; \
-	driver_entry->driver.is_enabled = apm_driver_##name##_is_enabled; \
-	driver_entry->driver.error_reporting = apm_driver_##name##_error_reporting; \
-	driver_entry->driver.want_event = apm_driver_##name##_want_event; \
-	driver_entry->driver.want_stats = apm_driver_##name##_want_stats; \
-	driver_entry->driver.is_request_created = 0; \
-	driver_entry->next = NULL; \
-	return driver_entry; \
+    apm_driver_entry * driver_entry; \
+    driver_entry = (apm_driver_entry *) malloc(sizeof(apm_driver_entry)); \
+    driver_entry->driver.process_event = apm_driver_##name##_process_event; \
+    driver_entry->driver.minit = apm_driver_##name##_minit; \
+    driver_entry->driver.rinit = apm_driver_##name##_rinit; \
+    driver_entry->driver.mshutdown = apm_driver_##name##_mshutdown; \
+    driver_entry->driver.rshutdown = apm_driver_##name##_rshutdown; \
+    driver_entry->driver.process_stats = apm_driver_##name##_process_stats; \
+    driver_entry->driver.is_enabled = apm_driver_##name##_is_enabled; \
+    driver_entry->driver.error_reporting = apm_driver_##name##_error_reporting; \
+    driver_entry->driver.want_event = apm_driver_##name##_want_event; \
+    driver_entry->driver.want_stats = apm_driver_##name##_want_stats; \
+    driver_entry->driver.is_request_created = 0; \
+    driver_entry->next = NULL; \
+    return driver_entry; \
 }
 
 PHP_MINIT_FUNCTION(apm);
@@ -370,6 +370,6 @@ ZEND_EXTERN_MODULE_GLOBALS(apm)
 # define add_assoc_long_compat(array, key, value) add_assoc_long_ex((array), (key), (sizeof(key)), (value));
 #endif
 
-void extract_data;
+void extract_data(void);
 #endif
 
